e9b5b1c25c4312007bf8a217ba5d03609404a001
unknown <tjburns102@gmail.com>
2020-02-29 12:16:53 -0600

    added new Choices cloctype, increased patch version and added example of using Choices

--- cloc/types.py
+++ cloc/types.py
@@ -4,6 +4,7 @@ from cloc.utils import defaultattr
 
 
 class BaseParamType(object):
+    __name__ = 'cloc.BaseParamType'
     basetype: Any
 
     def __init__(self, basetype: Any= None):
@@ -16,3 +17,15 @@ class BaseParamType(object):
         :return:
         """
         return self.basetype(value)
+
+class Choices(BaseParamType):
+    __name__ = 'cloc.Choices'
+
+    def __init__(self, choices:list, basetype: Any = Any):
+        super().__init__(basetype)
+        self.choices = set(choices)
+
+    def convert(self, value: str):
+        if value not in self.choices:
+            raise ValueError(f'Error: {value!r} was not found in choices: {", ".join(self.choices)!r}')
+        return value
